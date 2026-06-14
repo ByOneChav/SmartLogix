d86ae24d16979d477c056cd797a2309e29460862
lBlackDev <rolando3365@gmail.com>
2026-06-14 18:01:49 -0400

    9- Commit de Smartlogix con EKS

--- backend/msvc-authService/src/main/java/com/microservice/authservice/config/SecurityConfig.java
+++ backend/msvc-authService/src/main/java/com/microservice/authservice/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/actuator/**").permitAll()
                 .requestMatchers(
                     "/v3/api-docs/**",
                     "/swagger-ui/**",
