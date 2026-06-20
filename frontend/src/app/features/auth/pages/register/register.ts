6e3f1edc45b8354a19ed558b5c06426098a6590c
Alcindo Chavarria <139635361+ByOneChav@users.noreply.github.com>
2026-06-20 01:18:46 -0400

    agregar formulario de registro

--- frontend/src/app/features/auth/pages/register/register.ts
+++ frontend/src/app/features/auth/pages/register/register.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -19,21 +20,40 @@ export class Register {
   error = '';
   loading = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) { }
 
   register(): void {
     this.loading = true;
     this.error = '';
 
-    this.authService.register({ name: this.name, email: this.email, password: this.password, rol: this.rol })
+    this.authService.register({
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      rol: this.rol
+    })
       .subscribe({
         next: () => {
-          this.router.navigate(['/login']);
-        },
-        error: (err) => {
-          this.error = err.error?.message || 'Error al registrar usuario';
+
           this.loading = false;
+
+          Swal.fire({
+            icon: 'success',
+            title: 'Registro exitoso',
+            text: 'Ahora puedes iniciar sesión',
+            confirmButtonText: 'Continuar'
+          }).then(() => {
+            this.router.navigate(['/login']);
+          });
+        },
+
+        error: (err) => {
+
+          this.loading = false;
+
+          this.error = err.error?.message || 'Error al registrar usuario';
         }
       });
   }
 }
+
